e13dd8222350ac47794d67af73aeb114e864c531
Denny <155585746+dennysubke@users.noreply.github.com>
2024-10-16 21:08:25 +0200

    Update conf.php

--- denny-privatebin/conf.php
+++ denny-privatebin/conf.php
@@ -30,7 +30,7 @@ burnafterreadingselected = true
 defaultformatter = "plaintext"
 
 ; Set the template (you can choose other templates like "bootstrap-dark" if preferred)
-template = "bootstrap-page"
+template = "bootstrap5"
 
 ; Set a size limit for pastes (here 10 MB)
 sizelimit = 52428800
