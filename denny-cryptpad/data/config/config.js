937c7e51e054c7aa6a868c730bb63fb1524d9317
Denny <155585746+dennysubke@users.noreply.github.com>
2025-01-26 18:17:37 +0100

    Update config.js

--- denny-cryptpad/data/config/config.js
+++ denny-cryptpad/data/config/config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    httpUnsafeOrigin: "http://umbrel.local:3712",
-    httpSafeOrigin: "http://umbrel.local:3712",
+    httpUnsafeOrigin: "http://umbrel-2.local:3712",
+    httpSafeOrigin: "http://umbrel-2.local:3712",
     storageLimit: 100000000, // Speicherlimit in Bytes
-    adminEmail: "admin@umbrel.local", // Admin-E-Mail-Adresse
+    adminEmail: "admin@umbrel-2.local", // Admin-E-Mail-Adresse
     allowSubscriptions: true, // Subscriptions erlauben
-    sandboxDomain: "http://umbrel.local:3713", // Sandbox-Domain
+    sandboxDomain: "http://umbrel-2.local:3713", // Sandbox-Domain
     verbose: false // Protokollausgabe
 };
