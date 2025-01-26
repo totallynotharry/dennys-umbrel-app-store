b1a9c82af4445ac58500fc6b0bd384e8bf7714a2
Denny <155585746+dennysubke@users.noreply.github.com>
2025-01-26 20:08:38 +0100

    Delete denny-cryptpad/data/config directory

--- denny-cryptpad/data/config/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    httpUnsafeOrigin: "http://umbrel-2.local:3712",
-    httpSafeOrigin: "http://umbrel-2.local:3712",
-    storageLimit: 100000000, // Speicherlimit in Bytes
-    adminEmail: "admin@umbrel-2.local", // Admin-E-Mail-Adresse
-    allowSubscriptions: true, // Subscriptions erlauben
-    sandboxDomain: "http://umbrel-2.local:3713", // Sandbox-Domain
-    verbose: false // Protokollausgabe
-};
