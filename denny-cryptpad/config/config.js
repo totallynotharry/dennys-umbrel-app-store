cfa0f78ff4f2e203c4ac7a605b2b6f837fbc1457
Denny <155585746+dennysubke@users.noreply.github.com>
2025-01-26 20:57:02 +0100

    Update config.js

--- denny-cryptpad/config/config.js
+++ denny-cryptpad/config/config.js
@@ -1,28 +1,10 @@
 module.exports = {
-    // Die Hauptdomain (main domain), die von Clients verwendet wird
     httpUnsafeOrigin: 'http://umbrel-2.local:3712',
-
-    // Die Sandbox-Domain, die für Sicherheitszwecke getrennt ist
     httpSafeOrigin: 'http://umbrel-2.local:3713',
-
-    // Bindet den Server an eine bestimmte Adresse (nur lokal verfügbar machen)
-    httpAddress: 'umbrel-2.local',
-
-    // Port für die Hauptdomain
+    httpAddress: '0.0.0.0',
     httpPort: 3712,
-
-    // Port für die Sandbox-Domain (muss sich von httpPort unterscheiden)
     httpSafePort: 3713,
-
-    // Port für Websockets (sollte ebenfalls eindeutig sein)
     websocketPort: 3714,
-
-    // Maximal verwendete CPU-Kerne (optional, für bessere Performance anpassen)
-    maxWorkers: 4,
-
-    /* =====================
-     *        STORAGE
-     * ===================== */
     filePath: './datastore/',
     archivePath: './data/archive',
     pinPath: './data/pins',
@@ -32,17 +14,11 @@ module.exports = {
     blobStagingPath: './data/blobstage',
     decreePath: './data/decrees',
     logPath: './data/logs',
-
-    /* =====================
-     *       Logging
-     * ===================== */
-    logToStdout: true, // Aktiviert Konsolenprotokollierung
+    maxUploadSize: 20 * 1024 * 1024,
+    logToStdout: false,
     logLevel: 'info',
     logFeedback: false,
-
-    /* =====================
-     *     Debugging und Sicherheit
-     * ===================== */
     verbose: false,
-    installMethod: 'docker', // Indikator, dass Docker verwendet wird
+    installMethod: 'docker',
+    adminKeys: [],
 };
