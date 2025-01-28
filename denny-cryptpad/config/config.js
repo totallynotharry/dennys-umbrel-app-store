04bb355fcffa5e32236d41b421c679f4b7929b05
Denny <155585746+dennysubke@users.noreply.github.com>
2025-01-28 22:45:13 +0100

    Update config.js

--- denny-cryptpad/config/config.js
+++ denny-cryptpad/config/config.js
@@ -1,20 +1,20 @@
 module.exports = {
-    httpUnsafeOrigin: 'https://cryptpad.subke.cloud', // Public HTTPS-Domain
-    httpSafeOrigin: 'https://sandbox.subke.cloud',   // Public HTTPS-Domain für Sandbox
-    httpAddress: '0.0.0.0',                          // Hostname, wie in der Compose-Datei angegeben
-    filePath: './datastore/',                         // Datastore-Pfad, wie in den Volumes gemountet
-    archivePath: './data/archive',                    // Datenarchiv
-    pinPath: './data/pins',                           // Pins
-    taskPath: './data/tasks',                         // Tasks
-    blockPath: './block',                             // Block-Daten
-    blobPath: './blob',                               // Blob-Daten
-    blobStagingPath: './data/blobstage',              // Blob-Staging
-    decreePath: './data/decrees',                     // Dekrete
-    logPath: './data/logs',                           // Logs
-    maxUploadSize: 20 * 1024 * 1024,                   // 20 MB Upload-Limit
-    logToStdout: false,                               // Logging in Konsole deaktiviert
-    logLevel: 'info',                                 // Standard-Log-Level
-    logFeedback: false,                               // Feedback-Logs deaktiviert
-    verbose: false,                                   // Verbose-Logs deaktiviert
-    installMethod: 'docker',                          // Installationsmethode Docker
+    httpUnsafeOrigin: 'http://cryptpad.subke.cloud',  // Achte darauf, hier HTTP zu verwenden, wenn es sich um ein HTTP-Verbindung handelt
+    httpSafeOrigin: 'https://cryptpad.subke.cloud',   // Nutze HTTPS für die sichere Verbindung
+    httpAddress: '0.0.0.0',                           // Das bedeutet, dass es auf allen IPs lauscht
+    filePath: './datastore/',                          // Dein Dateispeicherort
+    archivePath: './data/archive',                     // Archivspeicherort
+    pinPath: './data/pins',                            // Pins Speicherort
+    taskPath: './data/tasks',                          // Tasks Speicherort
+    blockPath: './block',                              // Block-Daten
+    blobPath: './blob',                                // Blob-Daten
+    blobStagingPath: './data/blobstage',               // Blob-Staging-Pfad
+    decreePath: './data/decrees',                      // Dekrete
+    logPath: './data/logs',                            // Logs Pfad
+    maxUploadSize: 20 * 1024 * 1024,                    // Maximale Uploadgröße (20MB)
+    logToStdout: false,                                // Logs werden nicht in die Konsole ausgegeben
+    logLevel: 'info',                                  // Log-Level auf 'info' setzen
+    logFeedback: false,                                // Keine Feedback-Logs
+    verbose: false,                                    // Keine ausführlichen Logs
+    installMethod: 'docker',                           // Installationsmethode: Docker
 };
