dd625488dc7288e3dfdcd3887403339cf546f0e9
Denny <155585746+dennysubke@users.noreply.github.com>
2025-01-28 21:12:24 +0100

    Update config.js

--- denny-cryptpad/config/config.js
+++ denny-cryptpad/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     httpUnsafeOrigin: 'https://cryptpad.subke.cloud',
     httpSafeOrigin: 'https://sandbox.subke.cloud',
-    httpAddress: '0.0.0.0',
+    httpAddress: 'umbrel-2.local:3790',
     filePath: './datastore/',
     archivePath: './data/archive',
     pinPath: './data/pins',
