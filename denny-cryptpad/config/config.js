219a7fd36404949ae1c476f8570949ed098b8802
Denny <155585746+dennysubke@users.noreply.github.com>
2025-01-28 21:37:54 +0100

    Update config.js

--- denny-cryptpad/config/config.js
+++ denny-cryptpad/config/config.js
@@ -1,21 +1,36 @@
 module.exports = {
-    httpUnsafeOrigin: 'https://cryptpad.subke.cloud',
-    httpSafeOrigin: 'https://sandbox.subke.cloud',
-    httpAddress: '0.0.0.0',
-    filePath: './datastore/',
-    archivePath: './data/archive',
-    pinPath: './data/pins',
-    taskPath: './data/tasks',
-    blockPath: './block',
-    blobPath: './blob',
-    blobStagingPath: './data/blobstage',
-    decreePath: './data/decrees',
-    logPath: './data/logs',
-    maxUploadSize: 20 * 1024 * 1024,
-    logToStdout: false,
-    logLevel: 'info',
-    logFeedback: false,
-    verbose: false,
-    installMethod: 'docker',
-    adminKeys: [],
+  // Hauptdomain
+  httpUnsafeOrigin: 'https://cryptpad.subke.cloud',
+  httpAddress: '0.0.0.0',
+  httpPort: 3003,
+
+  // Sandbox-Domain
+  sandboxDomain: 'https://sandbox.subke.cloud',
+
+  // WebSocket-Konfiguration
+  websocket: {
+    enabled: true,
+    address: '0.0.0.0',
+    port: 3000, // Intern genutzt
+  },
+
+  // Speicherpfade
+  storage: {
+    data: '/cryptpad/data',
+    files: '/cryptpad/datastore',
+    temp: '/cryptpad/temp',
+    backup: '/cryptpad/backup',
+    blobs: '/cryptpad/blob',
+    pins: '/cryptpad/block',
+  },
+
+  // Administrator-Konto
+  admin: {
+    accountCreationDisabled: false,
+  },
+
+  // Sicherheits- und Performance-Einstellungen
+  limits: {
+    fileSize: 50000000, // Maximal 50 MB pro Datei
+  },
 };
